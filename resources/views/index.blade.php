e04a3818892ab4d764166d6871d98326fd3d00fc
Velerium <bondaniandrea@libero.it>
2021-09-23 18:21:22 +0200

    ...with pagination. I forgot to commit. Ugh.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,32 +5,35 @@
 <div class="container">
     
     @for ($i = 0; $i < count($articleList); $i += 6)
-
-    <div class="partial">
-        <div class="big">
-            <div class="big-content border">{{ $articleList[$i]->title }}</div>
-        </div>
-        <div class="big">
-            <div class="big-content">
-                <div class="small">{{ $articleList[$i+1]->title }}</div>
-                <div class="small">{{ $articleList[$i+2]->title }}</div>
+    <div class="small-container">
+        <div class="partial">
+            <div class="big">
+                <div class="big-content border"><h3>{{ $articleList[$i]->title }}</h3></div>
             </div>
-        </div>
-        
-    </div>
-    <div class="partial">
-        <div class="big">
-            <div class="big-content">
-                <div class="small">{{ $articleList[$i+3]->title }}</div>
-                <div class="small">{{ $articleList[$i+4]->title }}</div>
+            <div class="big">
+                <div class="big-content">
+                    <div class="small"><h3>{{ $articleList[$i+1]->title }}</h3></div>
+                    <div class="small"><h3>{{ $articleList[$i+2]->title }}</h3></div>
+                </div>
             </div>
+            
         </div>
-        <div class="big">
-            <div class="big-content border">{{ $articleList[$i+5]->title }}</div>
+        <div class="partial">
+            <div class="big">
+                <div class="big-content">
+                    <div class="small"><h3>{{ $articleList[$i+3]->title }}</h3></div>
+                    <div class="small"><h3>{{ $articleList[$i+4]->title }}</h3></div>
+                </div>
+            </div>
+            <div class="big">
+                <div class="big-content border"><h3>{{ $articleList[$i+5]->title }}</h3></div>
+            </div>
         </div>
     </div>
 
     @endfor
+
+    <div class="links">{{ $articleList->links() }}</div>
     
 </div>
 
